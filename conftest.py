6196833b1ef4d9164c263ea1a6a47290e542ba9a
karunakaransaranya <karunakaran.saranya@gmail.com>
2026-04-24 08:41:27 -0500

    My initial commit

---
+++ conftest.py
@@ -0,0 +1,56 @@
+import os
+import pytest
+from selenium import webdriver
+driver = None
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name",
+        action = "store",
+        default = "chrome",
+        help = "Browser name to run the tests",
+    )
+
+@pytest.fixture(scope="function")
+def browser_detail(request):
+    global driver
+    browser = request.config.getoption("browser_name")
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    yield driver
+    driver.quit()
+
+@pytest.hookimpl( hookwrapper=True )
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin( 'html' )
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr( report, 'extra', [] )
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr( report, 'wasxfail' )
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            reports_dir = os.path.join( os.path.dirname( __file__ ), 'reports' )
+            file_name = os.path.join( reports_dir, report.nodeid.replace( "::", "_" ) + ".png" )
+
+            # ✅ Relative path for HTML src attribute
+            relative_path = os.path.join("reports", report.nodeid.replace("::", "_") + ".png")
+            relative_path = relative_path.replace("\\", "/")  # ✅ fix backslashes for HTML
+
+            print( "file name is " + file_name )
+            _capture_screenshot( file_name )
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append( pytest_html.extras.html( html ) )
+        report.extras = extra
+
+
+def _capture_screenshot(file_name):
+    driver.get_screenshot_as_file(file_name)
